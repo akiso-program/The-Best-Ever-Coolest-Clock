453b9dc7eb09930adf8ef0cb42c6647e9e514574
SotaAkiyama <gemon0408@gmail.com>
2022-05-01 23:06:57 +0900

    クリックで0->9が順に出るように

--- script3.js
+++ script3.js
@@ -4,13 +4,21 @@
     'use strict';
 
     // variables
-    var gl, run, canvas, canvasWidth, canvasHeight, camera;
-    var targetImageData, imageWidth, imageHeight;
-    var mousePosition, isMousedown, acceleration;
+    var gl, run, canvas, canvasWidth, canvasHeight;
+    var imageWidth, imageHeight;
+    var mousePosition, isMousedown, clickCount, acceleration;
     canvasWidth = 512;
     canvasHeight = 512;
     imageWidth = 256;
     imageHeight = 256;
+    clickCount = 0;
+
+    var VBOArray = [[],[]];
+    var position = [];
+    var velocity = [];
+    var target = [[],[]];
+
+    var makefunc = [make_0,make_1,make_2,make_3,make_4,make_5,make_6,make_7,make_8,make_9];
 
     window.addEventListener('load', function(){
         var e = document.getElementById('info');
@@ -25,8 +33,14 @@
 
         // mousemove event
         canvas.addEventListener('mousedown', function(eve){
+            clickCount++;
             isMousedown = true;
             acceleration = 1.0;
+            makefunc[clickCount%makefunc.length](target[clickCount%2]);
+            VBOArray[0][1] = recreate_vio(VBOArray[0][1],velocity);
+            VBOArray[0][2] = recreate_vio(VBOArray[0][2],target[clickCount%2]);
+            VBOArray[1][1] = recreate_vio(VBOArray[1][1],velocity);
+            VBOArray[1][2] = recreate_vio(VBOArray[1][2],target[clickCount%2]);
         }, false);
         canvas.addEventListener('mouseup', function(eve){
             isMousedown = false;
@@ -80,41 +94,26 @@
             var vs = create_shader('vs_transform');
             var fs = create_shader('fs_transform');
             var prg = create_program_tf_separate(vs, fs, outVaryings);
-            var attLocation = [];
-            attLocation[0] = 0;
-            attLocation[1] = 1;
-            attLocation[2] = 2;
-            var attStride = [];
-            attStride[0] = 2;
-            attStride[1] = 2;
-            attStride[2] = 4;
-            var uniLocation = [];
-            uniLocation[0] = gl.getUniformLocation(prg, 'time');
-            uniLocation[1] = gl.getUniformLocation(prg, 'mousePos');
-            uniLocation[2] = gl.getUniformLocation(prg, 'isMove');
-            uniLocation[3] = gl.getUniformLocation(prg, 'acceleration');
+            var attStride = [2,2,2];
+            var uniLocation = [
+                gl.getUniformLocation(prg, 'time'),
+                gl.getUniformLocation(prg, 'mousePos'),
+                gl.getUniformLocation(prg, 'isMove'),
+                gl.getUniformLocation(prg, 'acceleration')
+            ];
 
             // feedback in shader
             vs = create_shader('vs_main');
             fs = create_shader('fs_main');
             var fPrg = create_program(vs, fs);
-            var fAttLocation = [];
-            fAttLocation[0] = 0;
-            fAttLocation[1] = 1;
-            fAttLocation[2] = 2;
-            var fAttStride = [];
-            fAttStride[0] = 2;
-            fAttStride[1] = 2;
-            fAttStride[2] = 4;
-            var fUniLocation = [];
-            fUniLocation[0] = gl.getUniformLocation(fPrg, 'vpMatrix');
-            fUniLocation[1] = gl.getUniformLocation(fPrg, 'move');
-            fUniLocation[2] = gl.getUniformLocation(fPrg, 'ambient');
+            var fAttStride = [2,2,2];
+            var fUniLocation = [
+                gl.getUniformLocation(fPrg, 'vpMatrix'),
+                gl.getUniformLocation(fPrg, 'move'),
+                gl.getUniformLocation(fPrg, 'ambient')
+            ];
 
             // vertices
-            var position = [];
-            var velocity = [];
-            var color = [];
             (function(){
                 var i, j, m;
                 var x, y;
@@ -125,21 +124,27 @@
                         position.push(x, -y);
                         m = Math.sqrt(x * x + y * y);
                         velocity.push(x / m, -y / m );
-                        color.push(0,0,0,0);
+                        if(j<imageWidth/2){
+                            target[0].push(0.5,0.0);
+                            target[1].push(0.0,0.5);
+                        }else{
+                            target[0].push(-0.5,0.0);
+                            target[1].push(0.0,-0.5);
+                        }
                     }
                 }
             })();
 
             // create vbo
-            var VBOArray = [
+            VBOArray = [
                 [
                     create_vbo(position),
                     create_vbo(velocity),
-                    create_vbo(color)
+                    create_vbo(target[0])
                 ], [
                     create_vbo(position),
                     create_vbo(velocity),
-                    create_vbo(color)
+                    create_vbo(target[0])
                 ]
             ];
 
@@ -188,7 +193,7 @@
                 gl.useProgram(prg);
 
                 // set vbo
-                set_attribute(VBOArray[countIndex], attLocation, attStride);
+                set_attribute(VBOArray[countIndex], attStride);
                 gl.bindBufferBase(gl.TRANSFORM_FEEDBACK_BUFFER, 0, VBOArray[invertIndex][0]);
                 gl.bindBufferBase(gl.TRANSFORM_FEEDBACK_BUFFER, 1, VBOArray[invertIndex][1]);
                 gl.bindBufferBase(gl.TRANSFORM_FEEDBACK_BUFFER, 2, VBOArray[invertIndex][2]);
@@ -221,7 +226,7 @@
                 gl.useProgram(fPrg);
 
                 // set vbo
-                set_attribute(VBOArray[invertIndex], fAttLocation, fAttStride);
+                set_attribute(VBOArray[invertIndex], fAttStride);
 
                 // push and render
                 gl.uniformMatrix4fv(fUniLocation[0], false, vpMatrix);
@@ -288,7 +293,8 @@
         }
     }
 
-    function create_vbo(data){
+    function recreate_vio(vbo, data){
+        gl.deleteBuffer(vbo);
         var vbo = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
@@ -296,25 +302,116 @@
         return vbo;
     }
 
-    function create_vbo_feedback(data){
+    function create_vbo(data){
         var vbo = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.DYNAMIC_COPY);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
         gl.bindBuffer(gl.ARRAY_BUFFER, null);
         return vbo;
     }
-
-    function set_attribute(vbo, attL, attS){
+    function set_attribute(vbo, attS){
         for(var i in vbo){
             gl.bindBuffer(gl.ARRAY_BUFFER, vbo[i]);
-            gl.enableVertexAttribArray(attL[i]);
-            gl.vertexAttribPointer(attL[i], attS[i], gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(i);
+            gl.vertexAttribPointer(i, attS[i], gl.FLOAT, false, 0, 0);
         }
     }
 
-    function set_attribute_base(vbo){
-        for(var i in vbo){
-            gl.bindBufferBase(gl.TRANSFORM_FEEDBACK_BUFFER, i, vbo[i]);
+    var seg_position = [[[-0.1,0.1],[0.1,0.1]],
+                        [[-0.1,0.0],[0.1,0.0]],
+                        [[-0.1,-0.1],[0.1,-0.1]]];
+    var upper_line = seg_position[0];
+    var center_line = seg_position[1];
+    var bottom_line = seg_position[2];
+    var upper_left_line = [seg_position[0][0],seg_position[1][0]];
+    var upper_right_line = [seg_position[0][1],seg_position[1][1]];
+    var bottom_left_line = [seg_position[1][0],seg_position[2][0]];
+    var bottom_right_line = [seg_position[1][1],seg_position[2][1]];
+
+    function make_0(target_buffer){
+        var line_num = 6;
+        make_line(upper_line,target_buffer,line_num,0);
+        make_line(upper_right_line,target_buffer,line_num,1);
+        make_line(bottom_right_line,target_buffer,line_num,2);
+        make_line(bottom_line,target_buffer,line_num,3);
+        make_line(bottom_left_line,target_buffer,line_num,4);
+        make_line(upper_left_line,target_buffer,line_num,5);
+    }
+    function make_1(target_buffer){
+        var line_num = 2;
+        make_line(upper_right_line,target_buffer,line_num,0);
+        make_line(bottom_right_line,target_buffer,line_num,1);
+    }
+    function make_2(target_buffer){
+        var line_num = 5;
+        make_line(upper_line,target_buffer,line_num,0);
+        make_line(upper_right_line,target_buffer,line_num,1);
+        make_line(center_line,target_buffer,line_num,2);
+        make_line(bottom_left_line,target_buffer,line_num,3);
+        make_line(bottom_line,target_buffer,line_num,4);
+    }
+    function make_3(target_buffer){
+        var line_num = 5;
+        make_line(upper_line,target_buffer,line_num,0);
+        make_line(upper_right_line,target_buffer,line_num,1);
+        make_line(center_line,target_buffer,line_num,2);
+        make_line(bottom_right_line,target_buffer,line_num,3);
+        make_line(bottom_line,target_buffer,line_num,4);
+    }
+    function make_4(target_buffer){
+        var line_num = 4;
+        make_line(upper_right_line,target_buffer,line_num,0);
+        make_line(bottom_right_line,target_buffer,line_num,1);
+        make_line(upper_left_line,target_buffer,line_num,2);
+        make_line(center_line,target_buffer,line_num,3);
+    }
+    function make_5(target_buffer){
+        var line_num = 5;
+        make_line(upper_line,target_buffer,line_num,0);
+        make_line(upper_left_line,target_buffer,line_num,1);
+        make_line(center_line,target_buffer,line_num,2);
+        make_line(bottom_right_line,target_buffer,line_num,3);
+        make_line(bottom_line,target_buffer,line_num,4);
+    }
+    function make_6(target_buffer){
+        var line_num = 5;
+        make_line(upper_left_line,target_buffer,line_num,0);
+        make_line(bottom_left_line,target_buffer,line_num,1);
+        make_line(bottom_line,target_buffer,line_num,2);
+        make_line(bottom_right_line,target_buffer,line_num,3);
+        make_line(center_line,target_buffer,line_num,4);
+    }
+    function make_7(target_buffer){
+        var line_num = 3;
+        make_line(upper_line,target_buffer,line_num,0);
+        make_line(upper_right_line,target_buffer,line_num,1);
+        make_line(bottom_right_line,target_buffer,line_num,2);
+    }
+    function make_8(target_buffer){
+        var line_num = 7;
+        make_line(upper_line,target_buffer,line_num,0);
+        make_line(upper_right_line,target_buffer,line_num,1);
+        make_line(bottom_right_line,target_buffer,line_num,2);
+        make_line(bottom_line,target_buffer,line_num,3);
+        make_line(bottom_left_line,target_buffer,line_num,4);
+        make_line(upper_left_line,target_buffer,line_num,5);
+        make_line(center_line,target_buffer,line_num,6);
+    }
+    function make_9(target_buffer){
+        var line_num = 5;
+        make_line(center_line,target_buffer,line_num,0);
+        make_line(upper_left_line,target_buffer,line_num,1);
+        make_line(upper_line,target_buffer,line_num,2);
+        make_line(upper_right_line,target_buffer,line_num,3);
+        make_line(bottom_right_line,target_buffer,line_num,4);
+    }
+
+    function make_line(point,buffer,step,count){
+        var start = point[0];
+        var vec = [point[1][0] - start[0],point[1][1] - start[1]];
+        for(var i=count*2;i<buffer.length;i+=step*2){
+            buffer[i] = start[0] + vec[0]/buffer.length * i;
+            buffer[i+1] = start[1] + vec[1]/buffer.length * i;
         }
     }
 })();
